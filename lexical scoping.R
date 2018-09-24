263d696d5eea4621168d2ecfc869210df8601a15
yiminglyu15 <43516974+yiminglyu15@users.noreply.github.com>
2018-09-24 08:57:50 +0800

    Add files via upload

---
+++ lexical scoping.R
@@ -0,0 +1,26 @@
+makeCacheMatrix <- function(x=matrix()){
+  m<-NULL
+  set<-function(y){
+    x<<-y 
+  }
+  get<-function() x
+  setinverse<-function(solve) m<<-solve
+  getinverse<-function() m
+  list(set=set,get=get,setinverse=setinverse,getM=getinverse)
+}
+#computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)){
+    message("TO cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setinverse(m)
+  m
+  ## Return a matrix that is the inverse of 'x'
+}
+
+ 
+   
